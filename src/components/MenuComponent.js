d49b5f078b4977d0d3cfe4a2dda350044601bd16
Somayeh mahjoob <somayehmahjob@gmail.com>
2021-10-10 20:09:39 +0100

    [Add] Show details dish with click on name

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -5,33 +5,66 @@ export class MenuComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      selectedDish: null
     };
+  }
+
+  onDishSelect(dish) {
+    this.setState({selectedDish: dish});
+  }
+
+  renderDish(dish) {
+    if(dish != null){
+      return (
+        <div className="flex">
+          <div className="flex-1">
+            <img src={dish.image} alt={dish.name} className="w-full" />
+          </div>
+          <div className="flex-1 p-4 text-left">
+            <h1 className="text-gray-900 font-extrabold mb-2 text-3xl">{dish.name}</h1>
+            <span className="text-gray-600 block mb-4 text-xl">{dish.category}</span>
+            <p className="text-gray-800 font-justify text-2xl">{dish.description}</p>
+          </div>
+        </div>
+      );
+    }
+    else{
+      return(
+        <div></div>
+      )
+    }
   }
   render() {
     const menuList = this.props.dishes.map((dish) =>{
       return (
         <div
           key={dish.id}
-          className="flex flex-wrap shadow-md rounded-md mt-5"
+          className="w-full aspect-w-1 aspect-h-1 bg-gray-900 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8"
         >
-          <div className="sm:w-full md:w-1/2 lg:w-1/3">
-            <img src={dish.image} alt={dish.name} className="" />
-          </div>
-          <div className="sm:w-full md:w-1/2 lg:w-2/3 text-left p-5">
-            <div className="md:text-3xl font-bold sm:text-2xl mb-2">{dish.name}</div>
-            <div className="md:text-xl sm:text-lg text-gray-500 sm:text-justify">{dish.description}</div>
+          <img
+            src={dish.image}
+            alt={dish.name}
+            className="w-full h-56 object-center object-cover  hover:opacity-75"
+          />
+          <div className="flex items-center px-6 py-3 bg-gray-900">
+            <h1
+              className="mx-3 text-lg font-bold text-white cursor-pointer"
+              onClick={() => this.onDishSelect(dish)}
+            >
+              {dish.name}
+            </h1>
           </div>
         </div>
       );
     })
     return (
-      <div className="container mt-2">
-        <div className="flex justify-start items-center">
-          <div className="p-2 m-2">
+      <div className="bg-white">
+        <div className="max-w-2xl mx-auto py-8 px-4 sm:py-16 sm-6 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Dishes</h2>
+          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {menuList}
           </div>
-          
+          <div className="mt-8">{this.renderDish(this.state.selectedDish)}</div>
         </div>
       </div>
     );
