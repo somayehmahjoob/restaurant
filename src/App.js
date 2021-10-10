801afbb86d8beaa80b209e7879b39b14e2bfd5b9
Somayeh mahjoob <somayehmahjob@gmail.com>
2021-10-10 07:52:25 +0100

    [Modify] show dishes with call state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import MenuComponent from './components/MenuComponent';
 import NavbarComponent from './components/NavbarComponent';
 import SlideShowComponent from './components/SlideShowComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <SlideShowComponent />
-      <MenuComponent />
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <SlideShowComponent />
+        <MenuComponent dishes={this.state.dishes} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
